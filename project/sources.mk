c59933ce1f1860e02d3a30886f0da1e66c523221
Ryan Mortenson <ryan.j.mortenson@gmail.com>
2017-05-24 21:44:00 -0600

    Initial submission of build system with test
    - TODO: Remove test file

---
+++ project/sources.mk
@@ -0,0 +1,37 @@
+
+# Set up some directories for later use
+APP_SRC_DIR=app/src
+APP_INC_DIR=app/inc
+APP_OUT=app/out
+
+# Build the output names for the application
+X86_APP_OUT=$(APP_OUT)/$(X86)
+ARM_APP_OUT=$(APP_OUT)/$(ARM)
+
+# Sources for the application
+NON_MAIN_SRC+=
+
+APP_SRC+= \
+	$(APP_SRC_DIR)/test.c
+
+TEST_SRC+= \
+	$(NON_MAIN_SRC) \
+
+# Make a src list without any directories to feed into the allasm/alli targets
+SRC_LIST = $(subst $(APP_SRC_DIR)/,,$(APP_SRC))
+
+# Build a list objects for each platform
+X86_OBJS = $(subst src,out/$(X86),$(patsubst %.c,%.o,$(APP_SRC)))
+ARM_OBJS = $(subst src,out/$(ARM),$(patsubst %.c,%.o,$(APP_SRC)))
+X86_TEST_OBJS = $(subst src,out/$(X86),$(patsubst %.c,%.o,$(TEST_SRC)))
+ARM_TEST_OBJS = $(subst src,out/$(ARM),$(patsubst %.c,%.o,$(TEST_SRC)))
+
+# Build a list of .d files to clean
+APP_DEPS += $(patsubst %.o,%.d, $(OBJS) $(25Z_OBJS) $(ARM_OBJS))
+
+# Add to List of items that need to be cleaned
+CLEAN+=$(ARM_OBJS) \
+       $(X86_OBJS) \
+       $(APP_DEPS) \
+       $(TEST_OBJS) \
+       $(APP_OUT)
