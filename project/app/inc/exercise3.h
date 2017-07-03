3764436014648ac2fd0f651c9394c878d88a89fc
rmortens <ryan.j.mortenson@gmail.com>
2017-07-03 14:54:29 -0600

    Fix build system

---
+++ project/app/inc/exercise3.h
@@ -0,0 +1,23 @@
+/** @file exercise3.h
+*
+* @brief Defines functions used for exercise3
+*
+*/
+
+#ifndef __EXERCISE3_H__
+#define __EXERCISE3_H__
+
+/*!
+* @brief Execute exercise 3 problem 2
+* @return 0
+*/
+int exercise3problem2();
+
+/*!
+* @brief Execute exercise 3 problem 4
+* @return 0
+*/
+int exercise3problem4();
+
+
+#endif /* __EXERCISE3_H__ */
